dcc6b19bad348f3a7d4635365300f1d0e93ce74c
Ben <benoit.zeller@gmail.com>
2020-12-16 12:40:10 +0100

    update template

--- template.rb
+++ template.rb
@@ -158,6 +158,7 @@ def add_friendly_id
   run "rails g migration AddSlugToUsers slug:uniq"
   inject_into_file 'app/models/user.rb', after: 'class User < ApplicationRecord' do
     <<-RUBY
+    \n
     extend FriendlyId
     friendly_id :pseudo, use: :slugged
     RUBY
@@ -167,6 +168,7 @@ end
 def add_i18n_params
   inject_into_file 'config/application.rb', after: 'config.load_defaults 6.0' do
     <<-RUBY
+    \n
     config.i18n.enforce_available_locales = true
     config.i18n.available_locales = %i[fr]
     config.i18n.default_locale = :fr
